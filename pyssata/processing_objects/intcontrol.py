6e0769f01cdab6d7c8922101141901e6fe6d2d58
FabioRossiArcetri <rossi76@gmail.com>
2024-08-07 15:04:46 +0200

    Create intcontrol.py

---
+++ pyssata/processing_objects/intcontrol.py
@@ -0,0 +1,53 @@
+class IntControl(IIRControl):
+    def __init__(self, gain, ff=None, delay=0):
+        iirfilter = int2iirfilter(gain, ff=ff)
+        
+        # Initialize IIRControl object
+        if not super().Init(iirfilter, delay=delay):
+            raise ValueError("Initialization of IIRControl failed")
+        
+        # Initialize BaseProcessingObj
+        if not BaseProcessingObj.Init(self, 'intcontrol', 'Integrator based Time Control'):
+            raise ValueError("Initialization of BaseProcessingObj failed")
+
+    @property
+    def ff(self):
+        return self._iirfilter.poles
+
+    @staticmethod
+    def revision_track():
+        return "$Rev$"
+
+    def cleanup(self):
+        super().cleanup()
+
+    def run_check(self, time_step, errmsg=""):
+        return True
+
+# Define the necessary functions used in IntControl
+def int2iirfilter(gain, ff=None):
+    # Implementation of the int2iirfilter function goes here
+    pass
+
+class BaseProcessingObj:
+    def Init(self, name, description):
+        # Implementation of the BaseProcessingObj.Init method goes here
+        return True
+
+class IIRControl:
+    def Init(self, iirfilter, delay=0):
+        # Implementation of the IIRControl.Init method goes here
+        return True
+
+    def cleanup(self):
+        # Implementation of the IIRControl.cleanup method goes here
+        pass
+
+    @property
+    def poles(self):
+        # Implementation of the IIRControl.poles property goes here
+        pass
+
+    def run_check(self, time_step, errmsg=""):
+        # Implementation of the IIRControl.run_check method goes here
+        return True
