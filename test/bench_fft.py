97de0f82574f9ce7225849bbbc241d7843e4f519
Alfio Puglisi <alfio.puglisi@inaf.it>
2024-10-04 11:06:00 +0200

    Added __name__ guard to bench_fft

--- test/bench_fft.py
+++ test/bench_fft.py
@@ -20,20 +20,21 @@ def test_one_size(s1, s2, s3, axes):
     return t1 - t0
 
 
-s1 = s2 = 1024
-size_list = range(1,60)
-elapsed_list = defaultdict(list)
+if __name__ == '__main__':
+    s1 = s2 = 1024
+    size_list = range(1,60)
+    elapsed_list = defaultdict(list)
 
-for n in size_list:
-    elapsed_list[f'{s1} x {s2} x n'].append(test_one_size(s1, s2, n, axes=(0, 1)))
+    for n in size_list:
+        elapsed_list[f'{s1} x {s2} x n'].append(test_one_size(s1, s2, n, axes=(0, 1)))
 
-for n in size_list:
-    elapsed_list[f'n x {s1} x {s2}'].append(test_one_size(n, s1, s2, axes=(1, 2)))
+    for n in size_list:
+        elapsed_list[f'n x {s1} x {s2}'].append(test_one_size(n, s1, s2, axes=(1, 2)))
 
-import matplotlib.pyplot as plt
-for k in elapsed_list:
-    plt.figure()
-    plt.plot(size_list, elapsed_list[k], '.-')
-    plt.xlabel(f'FFT size ({k})')
-    plt.ylabel('Computation time (s)')
-plt.show()
+    import matplotlib.pyplot as plt
+    for k in elapsed_list:
+        plt.figure()
+        plt.plot(size_list, elapsed_list[k], '.-')
+        plt.xlabel(f'FFT size ({k})')
+        plt.ylabel('Computation time (s)')
+    plt.show()
