c00f45632374ac9e04d280be0ea916ad98651bd4
guidoagapito <guido.agapito@inaf.it>
2024-10-29 16:45:14 +0100

    (GA) AtmoRandomPhase in subdirs

--- pyssata/calib_manager.py
+++ pyssata/calib_manager.py
@@ -17,7 +17,7 @@ class CalibManager(BaseParameterObj):
         super().__init__()
         self._subdirs = {
             'phasescreen': 'phasescreens/',
-            'AtmoCube': 'phasescreens/',
+            'AtmoRandomPhase': 'phasescreens/',
             'AtmoEvolution': 'phasescreens/',
             'slopenull': 'slopenulls/',
             'sn': 'slopenulls/',
