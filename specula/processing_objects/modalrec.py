834aef59a3a532c7774f47b243d27028a2045fb0
FabioRossiArcetri <rossi76@gmail.com>
2024-12-11 16:27:13 +0100

    (FRO) Cleaning POLC code.

--- specula/processing_objects/modalrec.py
+++ specula/processing_objects/modalrec.py
@@ -70,59 +70,25 @@ class Modalrec(BaseProcessingObj):
         self.recmat = recmat
         self.projmat = projmat
         self.intmat = intmat
-        self._polc = polc
-        self._layer_modes_list = None
-        self._past_step_list = []
+        self.polc = polc
+        self.layer_modes_list = None
+        self.past_step_list = []
 
-        self._modes = BaseValue('output modes from modal reconstructor', target_device_idx=target_device_idx)
-        self._pseudo_ol_modes = BaseValue('output POL modes from modal reconstructor', target_device_idx=target_device_idx)
-        self._modes_first_step = BaseValue('output (no projection) modes from modal reconstructor', target_device_idx=target_device_idx)
+        self.modes = BaseValue('output modes from modal reconstructor', target_device_idx=target_device_idx)
+        self.pseudo_ol_modes = BaseValue('output POL modes from modal reconstructor', target_device_idx=target_device_idx)
+        self.modes_first_step = BaseValue('output (no projection) modes from modal reconstructor', target_device_idx=target_device_idx)
 
         self.inputs['in_slopes'] = InputValue(type=Slopes, optional=True)
         self.inputs['in_slopes_list'] = InputList(type=Slopes, optional=True)
-        self.outputs['out_modes'] = self.out_modes
+        self.outputs['out_modes'] = self.modes
         self.outputs['out_pseudo_ol_modes'] = self.pseudo_ol_modes
         self.outputs['out_modes_first_step'] = self.modes_first_step
         
-        if self._polc:
+        if self.polc:
             self.out_comm = BaseValue('output commands from modal reconstructor', target_device_idx=target_device_idx)
             self.inputs['in_commands'] = InputValue(type=BaseValue, optional=True)
             self.inputs['in_commands_list'] = InputList(type=BaseValue, optional=True)
             self.outputs['out_comm'] = self.out_comm
-   
-
-# TODO
-    # @in_slopes.setter
-    # def in_slopes(self, value: Slopes):
-    #     self._slopes = value
-    #     if isinstance(value, Slopes):
-    #         size = value.size
-    #     if self._polc:
-    #         self._pseudo_ol_slopes = Slopes(size)
-
-    @property
-    def out_modes(self) -> BaseValue:
-        return self._modes
-
-    @property
-    def out_layer_modes_list(self):
-        return self._layer_modes_list
-
-    @property
-    def modes_first_step(self) -> BaseValue:
-        return self._modes_first_step
-
-    @modes_first_step.setter
-    def modes_first_step(self, value):
-        self._modes_first_step = value
-
-    @property
-    def pseudo_ol_slopes(self):
-        return self._pseudo_ol_slopes
-
-    @property
-    def pseudo_ol_modes(self) -> BaseValue:
-        return self._pseudo_ol_modes
 
     def trigger_code(self): # , slope_ptr=None
         if self.recmat.recmat is None:
@@ -136,58 +102,52 @@ class Modalrec(BaseProcessingObj):
         else:
             slopes = slopes.slopes
 
-        if self._modes_first_step.generation_time != self.current_time:
-            if self._polc:
-                commands = self.local_inputs['in_commands']
+        if self.modes_first_step.generation_time != self.current_time:
+            if self.polc:
+                commandsobj = self.local_inputs['in_commands']
                 commands_list = self.local_inputs['in_commands_list']
-                if commands is None:
+                if commandsobj is None:
+                    commandsobj = commands_list
                     commands = self.xp.hstack([x.commands for x in commands_list])
                 else:
-                    commands = commands.value
-                size = slopes.size
-                self._pseudo_ol_slopes = Slopes(size)
-                comm_slopes = self.compute_modes(self.intmat, self.xp.array(commands, dtype=self.dtype))
-                self._pseudo_ol_slopes.slopes += comm_slopes
-                self._pseudo_ol_slopes.generation_time = self.current_time
-                m = self.recmat.recmat @ self._pseudo_ol_slopes.slopes
-                self._pseudo_ol_modes.value = m
-                self._pseudo_ol_modes.generation_time = self.current_time
-                if not commands is None:
-                    self._modes_first_step.value = m - self.xp.asarray(commands)
+                    commands = self.xp.array( commandsobj.value, dtype=self.dtype)
+                pseudo_ol_slopes = Slopes(slopes.size)
+
+                if commandsobj is None or commands.shape == ():
+                    comm_slopes = self.xp.zeros_like(pseudo_ol_slopes.slopes)
+                    commands = self.xp.zeros(self.recmat.recmat.shape[0])
                 else:
-                    self._modes_first_step.value = m
+                    comm_slopes = self.intmat._intmat @ commands
+
+                pseudo_ol_slopes.slopes += comm_slopes
+                pseudo_ol_slopes.generation_time = self.current_time
+                self.pseudo_ol_modes.value = self.recmat.recmat @ pseudo_ol_slopes.slopes
+                self.pseudo_ol_modes.generation_time = self.current_time
+                self.modes_first_step.value = self.pseudo_ol_modes.value - commands
+
             else:
-                m = self.recmat.recmat @ slopes
-                self._modes_first_step.value = m
-            self._modes_first_step.generation_time = self.current_time
-            if self._layer_modes_list is not None:
-                for i, idx_list in enumerate(self._layer_idx_list):
-                    self._layer_modes_list[i].value = self._modes_first_step.value[idx_list]
-                    self._layer_modes_list[i].generation_time = self.current_time
+                self.modes_first_step.value = self.recmat.recmat @ slopes
+            self.modes_first_step.generation_time = self.current_time
+            if self.layer_modes_list is not None:
+                for i, idx_list in enumerate(self.layer_idx_list):
+                    self.layer_modes_list[i].value = self.modes_first_step.value[idx_list]
+                    self.layer_modes_list[i].generation_time = self.current_time
 
         if self.projmat is None:
-            if self._verbose:
-                n = len(self._modes_first_step.value)
-                print(f"(no projmat) first {min(6, n)} residual values: {self._modes_first_step.value[:min(5, n)]}")
-            self._modes.value = self._modes_first_step.value
-            self._modes.generation_time = self._modes_first_step.generation_time
+            if self.verbose:
+                n = len(self.modes_first_step.value)
+                print(f"(no projmat) first {min(6, n)} residual values: {self.modes_first_step.value[:min(5, n)]}")
+            self.modes.value = self.modes_first_step.value
+            self.modes.generation_time = self.modes_first_step.generation_time
         else:
-            mp = self.compute_modes(self.projmat, self._modes_first_step.ptr_value)
-            if self._verbose:
+            mp = self.compute_modes(self.projmat, self.modes_first_step.ptr_value)
+            if self.verbose:
                 print(f"first {min(6, len(mp))} residual values after projection: {mp[:min(5, len(mp))]}")
-            self._modes.value = mp
-            self._modes.generation_time = self.current_time
+            self.modes.value = mp
+            self.modes.generation_time = self.current_time
 
-        if self._polc and not commands is None:
-            self._modes.value -= commands
-
-
-    def compute_modes(self, matrix, slopes_vector):
-        if slopes_vector.shape==():
-            m = self.xp.zeros_like(self._pseudo_ol_slopes.slopes)
-        else:
-            m = self.intmat._intmat @ slopes_vector
-        return m
+        if self.polc and not commands is None:
+            self.modes.value -= commands
 
     def setup(self, loop_dt, loop_niters):
         super().setup(loop_dt, loop_niters)
@@ -199,11 +159,9 @@ class Modalrec(BaseProcessingObj):
             raise ValueError("Either 'slopes' or 'slopes_list' must be given as an input")
         if not self.recmat:
             raise ValueError("Recmat object not valid")
-        if self._polc:
+        if self.polc:
             if not self.intmat:
                 raise ValueError("Intmat object not valid")
-            #if not self._control_list:
-            #    raise ValueError("ControlList object not valid")
 
 
 
